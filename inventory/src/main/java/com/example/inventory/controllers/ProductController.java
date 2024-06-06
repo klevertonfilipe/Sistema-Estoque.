7fe200e633c2f6dc0c01fa047f5dd46981aa7606
Everton Farias <evertondefarias115@gmail.com>
2024-06-06 20:21:10 -0300

    feat: adicionando estrutura das pastas

--- inventory/src/main/java/com/example/inventory/controllers/ProductController.java
+++ inventory/src/main/java/com/example/inventory/controllers/ProductController.java
@@ -21,7 +21,6 @@ import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
